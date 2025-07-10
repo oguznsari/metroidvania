4cf8fc43fa7786eb3e97ac793524b7c45326e225
Oğuzhan SARI <oguzn.sari@gmail.com>
2025-07-10 17:46:47 +0300

    linked room1 and room2, finishing touches for initial screen

--- src/scenes/room1.js
+++ src/scenes/room1.js
@@ -1,17 +1,23 @@
-import { makeDrone } from "../entities/enemyDrone.js";
-import { makePlayer } from "../entities/player.js";
 import { makeBoss } from "../entities/enemyBoss.js";
+import { makeDrone } from "../entities/enemyDrone.js";
 import { makeCartridge } from "../entities/healthCartridge.js";
-import { healthBar } from "../ui/healthBar.js";
+import { makePlayer } from "../entities/player.js";
 import { state } from "../state/globalStateManager.js";
+import { healthBar } from "../ui/healthBar.js";
+
 import {
+  setMapColliders,
   setBackgroundColor,
   setCameraControls,
   setCameraZones,
-  setMapColliders,
+  setExitZones,
 } from "./roomUtils.js";
 
-export function room1(k, roomData) {
+export async function room1(
+  k,
+  roomData,
+  previousSceneData = { exitName: null }
+) {
   setBackgroundColor(k, "#a2aed5");
 
   k.camScale(4);
@@ -20,42 +26,49 @@ export function room1(k, roomData) {
 
   const roomLayers = roomData.layers;
 
-  // parent of all game object
   const map = k.add([k.pos(0, 0), k.sprite("room1")]);
-
-  let colliders = [];
-  let positions = [];
-  let cameras = [];
-
-  for (const layer of roomLayers) {
-    if (layer.name === "positions") {
-      positions.push(...layer.objects);
-      continue;
-    }
-
-    if (layer.name === "colliders") {
-      colliders.push(...layer.objects);
-    }
-
-    if (layer.name === "cameras") {
-      cameras.push(...layer.objects);
-    }
-  }
+  const colliders = roomLayers[4].objects;
 
   setMapColliders(k, map, colliders);
-  setCameraZones(k, map, cameras);
 
-  const player = k.add(makePlayer(k));
+  const player = map.add(makePlayer(k));
 
   setCameraControls(k, player, map, roomData);
 
+  const positions = roomLayers[5].objects;
   for (const position of positions) {
-    if (position.name === "player") {
+    if (position.name === "player" && !previousSceneData.exitName) {
       player.setPosition(position.x, position.y);
       player.setControls();
-      player.setEvents();
       player.enablePassthrough();
+      player.setEvents();
       player.respawnIfOutOfBounds(1000, "room1");
+      continue;
+    }
+
+    if (
+      position.name === "entrance-1" &&
+      previousSceneData.exitName === "exit-1"
+    ) {
+      player.setPosition(position.x, position.y);
+      player.setControls();
+      player.enablePassthrough();
+      player.setEvents();
+      player.respawnIfOutOfBounds(1000, "room1");
+      k.camPos(player.pos);
+      continue;
+    }
+
+    if (
+      position.name === "entrance-2" &&
+      previousSceneData.exitName === "exit-2"
+    ) {
+      player.setPosition(position.x, position.y);
+      player.setControls();
+      player.enablePassthrough();
+      player.setEvents();
+      player.respawnIfOutOfBounds(1000, "room1");
+      k.camPos(player.pos);
       continue;
     }
 
@@ -76,6 +89,13 @@ export function room1(k, roomData) {
       map.add(makeCartridge(k, k.vec2(position.x, position.y)));
     }
   }
+
+  const cameras = roomLayers[6].objects;
+
+  setCameraZones(k, map, cameras);
+
+  const exits = roomLayers[7].objects;
+  setExitZones(k, map, exits, "room2");
 
   healthBar.setEvents();
   healthBar.trigger("update");
