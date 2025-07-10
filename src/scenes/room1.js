a389a49b431f7b0244a528800d0acf1c42e4f079
Oğuzhan SARI <oguzn.sari@gmail.com>
2025-07-10 09:26:43 +0300

    Implemented logic to load and display the map

---
+++ src/scenes/room1.js
@@ -0,0 +1,18 @@
+import { setBackgroundColor } from "./roomUtils.js";
+
+export function room1(k, roomData) {
+  setBackgroundColor(k, "#a2aed5");
+
+  const roomLayers = roomData.layers;
+
+  // parent of all game object
+  const map = k.add([k.pos(0, 0), k.sprite("room1")]);
+
+  let colliders = [];
+  for (const layer of roomLayers) {
+    if (layer.name === "colliders") {
+      colliders.push(...layer.objects);
+      break;
+    }
+  }
+}
